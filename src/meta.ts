476c9c2228ba208bee7ac50898246745e1a6c944
myl7 <myl@myl.moe>
2022-04-22 21:53:31 +0800

    Change from ioredis to webdis

--- src/meta.ts
+++ src/meta.ts
@@ -1,35 +1,42 @@
 // Copyright (c) 2022 Distributed Network Disk Group
 // SPDX-License-Identifier: Apache-2.0
 
-import Redis from 'ioredis'
-
 export class MetaClient {
-  private redis: Redis
-
-  constructor(redisUrl: string) {
-    this.redis = new Redis(redisUrl)
-  }
+  constructor(private redisUrl: string) {}
 
   async servers() {
-    const ids = await this.redis.keys('server:*')
-    const metas = await this.redis.mget(...ids)
+    const keysRes = await fetch(`${this.redisUrl}/keys/${encodeURIComponent('server:*')}`)
+    if (keysRes.status != 200) {
+      throw new Error(`keys request failed: status = ${keysRes.status}`)
+    }
+    const ids = ((await keysRes.json()) as any).keys as string[]
+    const mgetRes = await fetch(`${this.redisUrl}/mget/${ids.map(id => encodeURIComponent(id)).join('/')}`)
+    if (mgetRes.status != 200) {
+      throw new Error(`mget request failed: status = ${mgetRes.status}`)
+    }
+    const metas = ((await mgetRes.json()) as any).mget as string[]
     return ids.map((id, i) => ({ id, ...JSON.parse(metas[i]!) } as ServerMeta & { id: string }))
   }
 
   async set(path: string, meta: FileMeta) {
-    await this.redis.set(`file:${path}`, JSON.stringify(meta))
+    const res = await fetch(
+      `${this.redisUrl}/set/${encodeURIComponent(`file:${path}`)}/${encodeURIComponent(JSON.stringify(meta))}`
+    )
+    if (res.status != 200) {
+      throw new Error(`set request failed: status = ${res.status}`)
+    }
   }
 
   async get(path: string) {
-    const meta = await this.redis.get(`file:${path}`)
-    if (meta == null) {
+    const res = await fetch(`${this.redisUrl}/get/${encodeURIComponent(`file:${path}`)}`)
+    if (res.status != 200) {
+      throw new Error(`get request failed: status = ${res.status}`)
+    }
+    const meta = (await ((await res.json()) as any).get) as string | undefined
+    if (!meta) {
       throw new Error(`File not exists: path = ${path}`)
     }
     return JSON.parse(meta) as FileMeta
-  }
-
-  async close() {
-    await this.redis.quit()
   }
 }
 
